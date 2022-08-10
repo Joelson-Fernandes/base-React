5b6a87d3cb1e2dc79a67c93cb1bd4482b134f8d9
Joelson Fernandes <fernandesjoelson97@gmail.com>
2022-08-10 16:41:13 -0300

    updated react router v6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import history from './services/history';
 import GlobalStyles from './styles/globalStyles';
 import Header from './components/header';
-import RoutesApp from './routes';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Header />
-        <RoutesApp />
-        <GlobalStyles />
-        <ToastContainer autoClose={3000} className="toast-container" />
-      </Router>
+      <Header />
+      <Outlet />
+      <GlobalStyles />
+      <ToastContainer autoClose={3000} className="toast-container" />
     </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import history from './services/history';
 import App from './App';
+
+// Routes
+import Page404 from './pages/page404';
+import Home from './pages/home';
+import Login from './pages/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter history={history}>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
